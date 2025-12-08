be27e6114fea8ed992965749dcf1fba20b8ea7b5
Ali <ajsllm37@gmail.com>
2025-12-08 14:37:48 +0200

    shaito-updated-welcome-page

--- php/welcome.php
+++ php/welcome.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 echo'
     <!DOCTYPE html>
     <html lang="en">
@@ -13,13 +13,11 @@ echo'
 
             <div class="topSide">
                 <div class="topLeft"><img class="logo" src="../images/logo.png"></div>
-                
-                <div class="topRight">       
-                        <a href="#aboutUs">About us&nbsp;&nbsp;</a>
-                        <a href="contactUs.php">&nbsp;&nbsp;Contact us&nbsp;&nbsp;</a>
-                        <a href="reviewUs.php">&nbsp;&nbsp;Review us</a>
-                        
-                        <button class="signin" onclick="window.location.href="signin.php" " >Sign in</button>
+                <div class="topRight">
+                    <a href="aboutUs.php">About us&nbsp;&nbsp;</a>
+                    <a href="contactUs.php">&nbsp;&nbsp;Contact us&nbsp;&nbsp;</a>
+                    <a href="reviewUs.php">&nbsp;&nbsp;Review us</a>
+                    <button class="signin"><a class="signinn" href="signIn.php">Sign in</a></button>
                 </div>
             </div>
 
@@ -36,14 +34,9 @@ echo'
             </div>
 
             <div class="middle2">
-                <div id="aboutUs"></div>
-                <div class="middle2-top">
-                    <h1>Our Mission</h1>
-                    <p>Empowering the next generation of developers through practice and community.</p>
-                </div>
-
+                
             </div>
-            
+
             <footer>
                 <div class="footer">
                     <div class="bottomLeft"><img src="../images/logo.png" style="width: 170px;height:95px;margin-top:-10px;"></div>
